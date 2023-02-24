b26fe03b6176876b940a74d3335ac6ec81594fbf
yeray.lopez <yeray.lopez@invesyde.com>
2023-02-24 10:17:29 +0100

    lazyload

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,8 @@
+
+export const LazyPage1 = () => {
+  return (
+    <h1>Lazy page 1</h1>
+  )
+}
+
+export default LazyPage1;
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,8 @@
+
+export const LazyPage3 = () => {
+  return (
+    <h1>Lazy page 3</h1>
+  )
+}
+
+export default LazyPage3;
