4909ecb1b4e56a714d2a46dedbf89d45be0b4471
yeray.lopez <yeray.lopez@invesyde.com>
2023-02-24 11:38:57 +0100

    sadfasdf

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,8 +1,8 @@
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  NavLink
+  NavLink,
+  Routes
 } from 'react-router-dom';
 
 import logo from '../logo.svg';
@@ -15,20 +15,20 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''} >Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''} >About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''} >Users</NavLink>
             </li>
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
+        <Routes>
           <Route path="/about">
             <h1>About</h1>
           </Route>
@@ -38,7 +38,7 @@ export const Navigation = () => {
           <Route path="/">
             <h1>Home</h1>
           </Route>
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
