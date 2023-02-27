d9670f0051ffdc15fae762da107a35fcd1d21cf2
yeray.lopez <yeray.lopez@invesyde.com>
2023-02-27 08:37:28 +0100

    sadfasf

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,44 +2,49 @@ import {
   BrowserRouter as Router,
   Route,
   NavLink,
-  Routes
+  Routes,
+  BrowserRouter
 } from 'react-router-dom';
 
 import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className="main-layout">
-        <nav>
-            <img src={ logo } alt="React Logo" />
-          <ul>
-            <li>
-              <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''} >Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''} >About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''} >Users</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <BrowserRouter>
+      <Router>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              <li>
+                <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''} >Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''} >About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''} >Users</NavLink>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/about">
+              <h1>About</h1>
+            </Route>
+            <Route path="/users">
+              <h1>Users</h1>
+            </Route>
+            <Route path="/">
+              <h1>Home</h1>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </BrowserRouter>
+
+
   );
 }
